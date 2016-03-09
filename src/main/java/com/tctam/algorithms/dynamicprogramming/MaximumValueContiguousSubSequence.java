03274df2b3512ecfd697d7d8df7fe160e9faf5f3
Tran Chi Tam <tranchitams3201@gmail.com>
2016-03-09 18:34:19 +0700

    - Add MaximumValueContiguousSubSequence

--- src/main/java/com/tctam/algorithms/dynamicprogramming/MaximumValueContiguousSubSequence.java
+++ src/main/java/com/tctam/algorithms/dynamicprogramming/MaximumValueContiguousSubSequence.java
@@ -7,10 +7,10 @@ import java.io.PrintWriter;
 import com.tctam.algorithms.utils.InputReader;
 
 /*
- 3 11 
- 1 3 5
+ 16
+ 2 -4 5 -8 4 -1 -1 1 1 1 -2 2 4 -6 9 -4
  */
-public class Coin {
+public class MaximumValueContiguousSubSequence {
     public static void main(String[] args) {
         InputStream inputStream = System.in;
         OutputStream outputStream = System.out;
@@ -24,26 +24,29 @@ public class Coin {
     static class Solve {
         public void solve(InputReader in, PrintWriter out) {
             int n = in.nextInt();
-            int m = in.nextInt();
-            int v[] = new int[n];
+            int a[] = new int[n];
+            int m[] = new int[n];
             for (int i = 0; i < n; i++) {
-                v[i] = in.nextInt();
+                a[i] = in.nextInt();
+                m[i] = a[i];
             }
+            int p = 0;
+            int q = 0;
+            int max = m[0];
 
-            int min[] = new int[m + 1];
-            for (int i = 0; i <= m; i++) {
-                min[i] = 9999;
-            }
-            min[0] = 0;
-
-            for (int i = 1; i <= m; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (v[j] <= i && min[i - v[j]] + 1 < min[i]) {
-                        min[i] = min[i - v[j]] + 1;
-                    }
+            for (int i = 1; i < n; i++) {
+                if (m[i - 1] > 0) {
+                    m[i] = m[i - 1] + a[i];
+                } else {
+                    p = i;
+                    m[i] = a[i];
+                }
+                if (m[i] > max) {
+                    q = i;
+                    max = m[i];
                 }
             }
-            out.println(min[m]);
+            out.println(String.format("%d %d %d", p + 1, q + 1, max));
         }
     }
 }
