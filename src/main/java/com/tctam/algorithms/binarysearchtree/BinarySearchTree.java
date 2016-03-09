8ab13575b518ea03ac296190946ae3fd3e63e4d4
tranchitam <tranchitams3201@gmail.com>
2016-03-09 23:45:36 +0700

    - Modify BinarySearchTree

--- src/main/java/com/tctam/algorithms/binarysearchtree/BinarySearchTree.java
+++ src/main/java/com/tctam/algorithms/binarysearchtree/BinarySearchTree.java
@@ -4,107 +4,177 @@ import com.tctam.algorithms.symboltable.SymbolTable;
 
 @SuppressWarnings({ "unchecked", "rawtypes" })
 public class BinarySearchTree<Key extends Comparable, Value> implements SymbolTable<Key, Value> {
-    private Node root;
+	private Node root;
 
-    private class Node {
-        private Key key;
-        private Value value;
-        private Node left;
-        private Node right;
-    }
+	private class Node {
+		private Key key;
+		private Value value;
+		private Node left;
+		private Node right;
+	}
 
-    public BinarySearchTree() {
+	public BinarySearchTree() {
 
-    }
+	}
 
-    public void put(Key key, Value value) {
-        root = put(root, key, value);
-    }
+	public void put(Key key, Value value) {
+		root = put(root, key, value);
+	}
 
-    private Node put(Node node, Key key, Value value) {
-        if (node == null) {
-            node = new Node();
-            node.key = key;
-            node.value = value;
-        } else {
-            int compare = key.compareTo(node.key);
-            if (compare < 0) {
-                node.left = put(node.left, key, value);
-            } else if (compare > 0) {
-                node.right = put(node.right, key, value);
-            } else {
-                node.value = value;
-            }
-        }
-        return node;
-    }
+	private Node put(Node x, Key key, Value value) {
+		if (x == null) {
+			x = new Node();
+			x.key = key;
+			x.value = value;
+		} else {
+			int compare = key.compareTo(x.key);
+			if (compare < 0) {
+				x.left = put(x.left, key, value);
+			} else if (compare > 0) {
+				x.right = put(x.right, key, value);
+			} else {
+				x.value = value;
+			}
+		}
+		return x;
+	}
 
-    public Value get(Key key) {
-        Node x = root;
-        while (x != null) {
-            int compare = key.compareTo(x.key);
-            if (compare < 0) {
-                x = x.left;
-            } else if (compare > 0) {
-                x = x.right;
-            } else {
-                return x.value;
-            }
-        }
-        return null;
-    }
+	public Value get(Key key) {
+		return get(root, key);
+	}
 
-    public void preOrder() {
-        preOrder(root);
-    }
+	private Value get(Node x, Key key) {
+		if (x == null) {
+			return null;
+		}
+		int compare = key.compareTo(x.key);
+		if (compare < 0) {
+			return get(x.left, key);
+		} else if (compare > 0) {
+			return get(x.right, key);
+		} else {
+			return x.value;
+		}
+	}
 
-    private void preOrder(Node node) {
-        if (node != null) {
-            System.out.print(node.key + " ");
-            preOrder(node.left);
-            preOrder(node.right);
-        }
-    }
+	public void delete(Key key) {
+		root = delete(root, key);
+	}
 
-    public void inOrder() {
-        inOrder(root);
-    }
+	private Node delete(Node x, Key key) {
+		if (x == null) {
+			return null;
+		}
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0) {
+			x.left = delete(x.left, key);
+		} else if (cmp > 0) {
+			x.right = delete(x.right, key);
+		} else {
+			if (x.right == null) {
+				return x.left;
+			}
+			if (x.left == null) {
+				return x.right;
+			}
 
-    private void inOrder(Node node) {
-        if (node != null) {
-            inOrder(node.left);
-            System.out.print(node.key + " ");
-            inOrder(node.right);
-        }
-    }
+			Node t = x;
+			x = min(t.right);
+			x.right = deleteMin(t.right);
+			x.left = t.left;
+		}
 
-    public void postOrder() {
-        postOrder(root);
-    }
+		return x;
+	}
 
-    private void postOrder(Node node) {
-        if (node != null) {
-            postOrder(node.left);
-            postOrder(node.right);
-            System.out.print(node.key + " ");
-        }
-    }
+	private Node min(Node x) {
+		if (x.left == null) {
+			return x;
+		} else {
+			return min(x.left);
+		}
+	}
 
-    public static void main(String[] args) {
-        BinarySearchTree<String, String> bst = new BinarySearchTree<String, String>();
-        bst.put("S", "S");
-        bst.put("E", "E");
-        bst.put("X", "X");
-        bst.put("A", "A");
-        bst.put("C", "C");
-        bst.put("R", "R");
-        bst.put("H", "H");
-        bst.put("M", "M");
+	public void deleteMin() {
+		root = deleteMin(root);
+	}
 
-        bst.preOrder();
-        System.out.println("\n---------------");
-        bst.inOrder();
-        System.out.println("\n---------------");
-        bst.postOrder();
-    }
+	private Node deleteMin(Node x) {
+		if (x.left == null) {
+			return x.right;
+		}
+		x.left = deleteMin(x.left);
+		return x;
+	}
+
+	public void deleteMax() {
+		root = deleteMax(root);
+	}
+
+	private Node deleteMax(Node x) {
+		if (x.right == null) {
+			return x.left;
+		}
+		x.right = deleteMax(x.right);
+		return x;
+	}
+
+	public void preOrder() {
+		preOrder(root);
+	}
+
+	private void preOrder(Node node) {
+		if (node != null) {
+			System.out.print(node.key + " ");
+			preOrder(node.left);
+			preOrder(node.right);
+		}
+	}
+
+	public void inOrder() {
+		inOrder(root);
+	}
+
+	private void inOrder(Node node) {
+		if (node != null) {
+			inOrder(node.left);
+			System.out.print(node.key + " ");
+			inOrder(node.right);
+		}
+	}
+
+	public void postOrder() {
+		postOrder(root);
+	}
+
+	private void postOrder(Node node) {
+		if (node != null) {
+			postOrder(node.left);
+			postOrder(node.right);
+			System.out.print(node.key + " ");
+		}
+	}
+
+	public static void main(String[] args) {
+		BinarySearchTree<String, String> bst = new BinarySearchTree<String, String>();
+		bst.put("S", "S");
+		bst.put("E", "E");
+		bst.put("X", "X");
+		bst.put("A", "A");
+		bst.put("C", "C");
+		bst.put("R", "R");
+		bst.put("H", "H");
+		bst.put("M", "M");
+
+		bst.preOrder();
+		System.out.println("\n---------------");
+		bst.inOrder();
+		System.out.println("\n---------------");
+		bst.postOrder();
+
+		bst.delete("E");
+		System.out.println("\n---------------");
+		bst.preOrder();
+	}
+
 }
--- src/main/java/com/tctam/algorithms/symboltable/SymbolTable.java
+++ src/main/java/com/tctam/algorithms/symboltable/SymbolTable.java
@@ -5,4 +5,10 @@ public interface SymbolTable<Key extends Comparable, Value> {
     public abstract void put(Key key, Value value);
 
     public abstract Value get(Key key);
+    
+    public abstract void delete(Key key);
+    
+    public abstract void deleteMin();
+    
+    public abstract void deleteMax();
 }
